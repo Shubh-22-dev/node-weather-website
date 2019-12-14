f17daae8cdbac6bd8fb7309e428da9d7f3e11082
Shubhkarman Singh <shubhkarmansingh45@gmail.com>
2019-12-14 20:00:01 +0530

    Updated Weather Info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,8 +8,9 @@ const forecast = (lat,long,callback) => {
             callback('Unable to connect to weather service!', undefined)
         }else if(body.error){
             callback('Unable to find location!', undefined)
-        }else{
-            var result = body.daily.data[0].summary + ' It is currently '+ body.currently.temperature +' degrees out. There is a '+ body.currently.precipProbability +'% chance of rain.'
+        }else{            
+            var todayTemp = "Today's highest temperature is " + body.daily.data[0].temperatureHigh + ' and lowest temperature is ' + body.daily.data[0].temperatureLow +'.'
+            var result = body.daily.data[0].summary + ' It is currently '+ body.currently.temperature +' degrees out. There is a '+ body.currently.precipProbability +'% chance of rain.' + todayTemp
             callback(undefined, result)
         }
 
